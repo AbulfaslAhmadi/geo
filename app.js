29c234c2daa6edf3855e9deca1325fd76f75fde9
Abulfasl Ahmadi <abulfasl.a@icloud.com>
2023-03-23 21:26:02 +0100

    v6

---
+++ app.js
@@ -0,0 +1,4 @@
+let width = window.innerWidth;
+if(width > 425) {
+    alert("Es tut uns leid, aber diese Website wurde hauptsächlich für mobile Geräte entwickelt und kann daher auf Desktop- oder Tablet-Geräten nicht optimal dargestellt werden. Wenn du trotzdem fortfahren möchtest, kannst du die Website weiterhin nutzen. Bitte beachte jedoch, dass einige Elemente möglicherweise nicht korrekt angezeigt werden oder nicht wie erwartet funktionieren. Wir empfehlen daher, diese Website auf einem mobilen Gerät zu besuchen, um das beste Nutzererlebnis zu erhalten.")
+}
